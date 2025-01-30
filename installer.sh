0b961f223ea55f71f5546a396a9b76f5f247763b
~~~FluffyFox~~~ <84251725+FluffyFox337@users.noreply.github.com>
2025-01-30 17:10:06 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,4 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
+pkg update && pkg upgrade -y
+
 architecture=""
 case $(uname -m) in
     armv7*)   architecture="arm" ;;
@@ -6,7 +8,8 @@ case $(uname -m) in
     *)   printf "The architecture of your CPU is not supported. Please build required binaries from the sources: https://github.com/drygdryg/OneShot#termux"; exit
 esac
 pkg install -y root-repo 
-pkg install -y wget tsu python wpa-supplicant iw
+pkg install -y wget tsu python wpa-supplicant iw openssl
+pkg install -y termux-tools ncurses-utils
 
 # keep upstream
 wget https://github.com/FluffyFox337/oneshot_installer-termux/raw/refs/heads/master/binaries/$architecture/pixiewps -O $PREFIX/bin/pixiewps
@@ -15,6 +18,7 @@ chmod +x $PREFIX/bin/pixiewps
 wget https://raw.githubusercontent.com/FluffyFox337/oneshot/refs/heads/master/oneshot.py
 # keep upstream
 wget https://raw.githubusercontent.com/FluffyFox337/oneshot/refs/heads/master/vulnwsc.txt
+wget https://raw.githubusercontent.com/FluffyFox337/oneshot/refs/heads/master/pins.csv
 
 chmod +x ./oneshot.py
 
